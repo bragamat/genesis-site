edd6441c6f4ba9ac55ee55982363ab0f1b25c3d5
Mateus Braga <mateuscbraga@gmail.com>
2021-12-27 13:30:14 -0300

    adjusted icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Example() {
               <div className="absolute inset-0">
                 <Image
                   className="h-full w-full object-cover"
-                  src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+                  src="/img/first-image-gun-02.jpg"
                   alt="People working on laptops"
                   layout="fill"
                 />
@@ -26,34 +26,13 @@ export default function Example() {
               <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                 <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
                   <span className="block text-white">
-                    Tire sua autorização e certificado
-                  </span>
-                  <span className="block text-indigo-200">
-                    para porte de arma
+                    Assessoria completa para você se tornar um Atirador Esportivo ou a obter a Posse de Arma de Fogo
                   </span>
                 </h1>
                 <p className="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">
-                  Com a Gênesis Assessoria fica tudo mais fácil
                 </p>
                 <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                   <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-                    {/* <Link href="/contato" passHref> */}
-                    <a
-                      href="https://web.whatsapp.com/send?phone=5511932155396"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <a className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8">
-                        Contato
-                      </a>
-                    </a>
-                    {/* </Link> */}
-                    <a
-                      href="#"
-                      className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
-                    >
-                      Live demo
-                    </a>
                   </div>
                 </div>
               </div>
