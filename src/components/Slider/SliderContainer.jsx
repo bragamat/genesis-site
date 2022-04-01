e8a2d3a0b49ca2f99ef4dfc9e833691aa27d2b9c
Mateus Braga <mateuscbraga@gmail.com>
2022-03-31 21:18:13 -0300

    adjusted sliders

--- src/components/Slider/SliderContainer.jsx
+++ src/components/Slider/SliderContainer.jsx
@@ -1,8 +1,47 @@
+import { useState, useEffect } from 'react'
+import classNames from 'classnames'
 import Slide from './index'
 
 export default function SliderContainer({ images }) {
+  const [screenWidth, setScreenWidth] = useState(0)
+
+  useEffect(() => {
+    function setWidth() {
+      console.log('width? ', window.innerWidth)
+      setScreenWidth(() => window.innerWidth)
+    }
+
+    if(window) {
+      window?.addEventListener('resize', () => setWidth())
+    }
+
+    return () => window?.removeEventListener('resize', () => setWidth())
+  }, [])
+
   return (
-    <div className="carousel">
+    <div className={classNames("carousel")}
+      style={{
+        ...(() => {
+          if(screenWidth === 0) {
+            return {}
+          }
+            if(screenWidth <= 521) {
+              return { maxWidth: '70%' }
+            }
+            if(screenWidth <= 765) {
+              return { maxWidth: '72%' }
+            }
+
+            if(screenWidth <= 1024) {
+              console.log('hey', screenWidth)
+              return { maxWidth: '66%' }
+            }
+
+          return {
+            maxWidth: '100%'
+          }
+        })()
+      }}>
       {
         images.map(({src, name, ...rest}, index, array) => (
           <Slide
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
     require('@tailwindcss/forms'),
